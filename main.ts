f0a63e46940927f91023e07bf48911c722b0b57f
deepthakannan <deepthakannan@gmail.com>
2018-05-06 12:59:22 -0400

    first observable and observer

--- main.ts
+++ main.ts
@@ -1,1 +1,26 @@
-alert("Rx !!!");
+import {Observable, Observer, from} from 'rxjs'
+
+let colors = ["violet", "indigo", "blue", null, undefined];
+
+let source = from(colors);
+
+class ColorObserver implements Observer<string> {
+
+    constructor(private name) {
+
+    }
+    next (value) {
+        console.log(`ColorObserver ${this.name}: ${value}`);
+    }
+
+    error (error) {
+        console.log(error);
+    }
+
+    complete () {
+        console.log("Complete");
+    }
+}
+
+source.subscribe(new ColorObserver(1))
+source.subscribe(new ColorObserver(2))
